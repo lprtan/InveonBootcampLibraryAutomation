4397e20600f1b44abb092413ea39b0b6de4987e9
Alperen Tan <lprntan@gmail.com>
2024-12-11 21:29:24 +0300

    Layers added

---
+++ InveonBootcamp/DataAccessLayer/Context/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Context
+{
+    public class AppDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-J7SKD7G;database=LibraryAutomationDb; integrated security=true;TrustServerCertificate=True;MultipleActiveResultSets=True");
+            optionsBuilder.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+        }
+    }
+}
